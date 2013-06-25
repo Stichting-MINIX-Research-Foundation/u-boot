bf18f9cddc61af8b3630690ae65f43422bf6f3ff
Kees Jongenburger <kees.jongenburger@gmail.com>
2013-06-25 09:24:25 +0200

    update doit.sh to also copy the image

--- doit.sh
+++ doit.sh
@@ -1,15 +1,23 @@
 git clean -f -x -d -e doit.sh
 export CROSS_COMPILE=arm-none-eabi-
 
+#
+# Remove the build *after* the clea and before
+# we start building (e.g. the build directory
+# is regenerated.
+#
+rm -rf build
+
 for i in am335x_evm omap3_beagle
 do
 (
 	make ${i}_config
 	make
-	sha1sum u-boot MLO spl/u-boot-spl.bin  > u-boot.sha1
+	sha1sum u-boot.img u-boot MLO spl/u-boot-spl.bin  > u-boot.sha1
 	mkdir -p build/$i/spl
 	cp MLO build/$i/
 	cp u-boot build/$i/
+	cp u-boot.img build/$i/
 	cp spl/u-boot-spl.bin build/$i/spl/
 	cp u-boot.sha1 build/$i/
 	git clean -f -x -d -e doit.sh -e build
